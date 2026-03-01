0bf12078ddc35081a11a38c4c9a66e785766e3ae
Fardin-Dange <fardindange01@gmail.com>
2026-03-01 13:30:21 +0530

    fix root mapping

--- src/main/java/com/fd/controller/EmpController.java
+++ src/main/java/com/fd/controller/EmpController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -13,6 +14,7 @@ import com.fd.entity.Employee;
 import com.fd.service.EmpService;
 
 @Controller
+@RequestMapping("/")
 public class EmpController {
 
     private EmpService service;
@@ -21,10 +23,10 @@ public class EmpController {
         this.service = service;
     }
     
-    @GetMapping("/")
+   @GetMapping({"/", "/index"})
     public String home() {
-        return "index"; // index.jsp
-    }
+    return "index";
+}
 
     // ================= LOGIN =================
     @PostMapping("/dashboard")
