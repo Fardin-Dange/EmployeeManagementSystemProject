59f85aa92d4e66c1a01fe3a6a523d701f052e743
Fardin-Dange <fardindange01@gmail.com>
2026-03-01 12:12:00 +0530

    fix: render 404 forward mapping

--- src/main/java/com/fd/SpringMvcEmsProjectApplication.java
+++ src/main/java/com/fd/SpringMvcEmsProjectApplication.java
@@ -2,13 +2,19 @@ package com.fd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringMvcEmsProjectApplication {
+public class SpringMvcEmsProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcEmsProjectApplication.class, args);
-		System.err.println("App Started");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringMvcEmsProjectApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcEmsProjectApplication.class, args);
+        System.err.println("App Started");
+    }
 }
